a8a0ae84e5a231d1b7f0acedc89721c2a51da554
Areeb Siddiqui <areeb40@hotmail.com>
2019-03-06 00:49:10 +0500

    setup

--- forGuestBook.js
+++ forGuestBook.js
@@ -1,7 +1,9 @@
+
 const express = require("express");
 const http = require("http");
 const logger = require("morgan");
 const path = require("path");
+const mongoose=require('mongoose');
 const bodyParser = require("body-parser");
 var port = process.env.PORT||3000;
 const app = express();
@@ -11,9 +13,50 @@ var entries = [];
 app.locals.entries = entries;
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
+mongoose.connect('mongodb://abc123:mybis@cluster0-shard-00-00-s8hvh.mongodb.net:27017,cluster0-shard-00-01-s8hvh.mongodb.net:27017,cluster0-shard-00-02-s8hvh.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',{ useNewUrlParser: true })
+.catch((e) => {
+    console.log("catch error: ", e)
+})
 
+
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function () {//connected
+console.log("Mongoose is connected");
+})
+
+mongoose.connection.on('disconnected', function () {//disconnected
+console.log("Mongoose is disconnected");
+process.exit(1);
+
+});
+
+mongoose.connection.on('error', function (err) {//any error
+console.log('Mongoose connection error: ', err);
+process.exit(1);
+})
+
+process.on('SIGINT', function () {/////this function will run jst before app is closing
+console.log("app is terminating");
+mongoose.connection.close(function () {
+    console.log('Mongoose default connection closed');
+    process.exit(0);
+});
+});
+
+var userSchema = new mongoose.Schema({
+    "title": String,
+    "body": String
+    
+
+});
+var userModel = mongoose.model("user", userSchema);
+
+
+
+//--------- main app
 app.get("/", (req, res, next) => {
     res.render("index");
+  
 });
 
 app.get("/new-entry", (req, res, next) => {
@@ -26,12 +69,24 @@ app.post("/new-entry",(req,res,next)=>{
             res.status(403).send("Entires must have title and body");
             return;
         }
-        entries.push({
+        
+        var newUser = {
+            title: req.body.title,
+            body: req.body.body
+        };
+        new userModel(newUser)
+            .save()
+            .then(s => {
+                res.send("Details Saved");
+            })
+    
+            .catch(err => { console.log(err) });
+        /*entries.push({
             title:req.body.title,
             content:req.body.body,
             published: new Date()
 
-        });
+        });*/
         res.redirect("/");
 });
 
